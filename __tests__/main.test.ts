0bed9bce60373bf221d4121419871f26c2aa31b4
Stefan Maric <me@stefanmaric.com>
2020-10-02 20:34:49 +0200

    Fix tests and workflow

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -2,7 +2,6 @@ import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 
-// shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
   const ip = path.join(__dirname, '..', 'dist', 'main.js')
   const options: cp.ExecSyncOptions = {
